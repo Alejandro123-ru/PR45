996831443df464bf05a01a7fe401460d221684d0
gobdanov <gobdanov@inbox.ru>
2026-04-02 16:46:25 +0500

    работан контекст для загрузки данных. Лисицкий

---
+++ Pr45_Api_Lisitskiy/Context/TaskContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pr45_Api_Lisitskiy.Models;
+
+namespace Pr45_Api_Lisitskiy.Context
+{
+    public class TaskContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+        public TaskContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql("server=local;uid=root;database=TaskManager", new MySqlServerVersion(new Version(8, 0, 11)));
+    }
+}
