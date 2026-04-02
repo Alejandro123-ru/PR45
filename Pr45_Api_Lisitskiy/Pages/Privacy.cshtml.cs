ab231d0748f1bb8e52da6e0336222ab9f8dab2f7
gobdanov <gobdanov@inbox.ru>
2026-04-02 16:08:39 +0500

    Добавьте файлы проекта.

---
+++ Pr45_Api_Lisitskiy/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pr45_Api_Lisitskiy.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
