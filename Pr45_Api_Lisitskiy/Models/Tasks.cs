8eef081d7e7a6ab9cd5994c1e9429520b1adf941
gobdanov <gobdanov@inbox.ru>
2026-04-02 16:38:00 +0500

    Разработана модель данных. Лисицкий

---
+++ Pr45_Api_Lisitskiy/Models/Tasks.cs
@@ -0,0 +1,36 @@
+﻿namespace Pr45_Api_Lisitskiy.Models
+{
+    /// <summary>
+    /// Класс задач
+    /// </summary>
+    public class Tasks
+    {
+        /// <summary>
+        /// Код задачи
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Наименование задачи
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Приоритет задачи
+        /// </summary>
+        public string Priority { get; set; }
+        /// <summary>
+        /// Дата выполнения задачи
+        /// </summary>
+        public DateTime DateExecute { get; set; }
+        /// <summary>
+        /// Комментарий к задаче
+        /// </summary>
+        public string Comment { get; set; }
+        /// <summary>
+        /// Выполнение задачи
+        /// </summary>
+        public bool Done { get; set; }
+
+    }
+}
+
+
