b659343b22d17db39f21a4470f1c08bfb2c4e163
anekk <aneckalarionova@gmail.com>
2026-04-04 14:06:45 +0500

    Создание модели и контекста пользователя. Лисицкий

---
+++ Pr45_Api_Lisitskiy/Context/UsersContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Pr45_Api_Lisitskiy.Models;
+
+namespace Pr45_Api_Lisitskiy.Context
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UsersContext()
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;uid=root;database=TaskManager", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
---
+++ Pr45_Api_Lisitskiy/Models/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace Pr45_Api_Lisitskiy.Models
+{
+    public class Users
+    {
+        public int Id {  get; set; }
+        public string Login {  get; set; }
+        public string Password {  get; set; }
+    }
+}
